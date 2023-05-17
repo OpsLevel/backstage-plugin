60b2844e68bb28a41d8afeedf06142a89d860574
Sergio <sergio@opslevel.com>
2023-05-17 15:05:09 -0400

    Clarify tests with  ApexCharts

--- src/test/OverallMaturityCategoryBreakdown.test.tsx
+++ src/test/OverallMaturityCategoryBreakdown.test.tsx
@@ -24,6 +24,13 @@ describe('OverallMaturityCategoryBreakdown', () => {
     unobserve() {}
   }
 
+  function bandaidsForApexCharts() {
+    // See issue - https://github.com/ZeeCoder/use-resize-observer/issues/40
+    global.ResizeObserver = require('resize-observer-polyfill');
+    // Fixes an ugly warning in Apexcharts
+    global.SVGElement.prototype.getBBox = () => ({x: 0, y: 0 });
+  }
+
   it('renders a progress bar in loading mode', () => {
     const wrapper = mount(<OverallMaturityCategoryBreakdown loading levels={[]} categoryLevelCounts={[]}/>);
 
@@ -43,14 +50,7 @@ describe('OverallMaturityCategoryBreakdown', () => {
   });
 
   it('renders a bar chart when there is data', () => {
-    // @ts-ignore
-    window.ResizeObserver = ResizeObserver;
-    // @ts-ignore
-    window.SVGElement.prototype.getBBox = () => ({
-      x: 0,
-      y: 0
-    });
-
+    bandaidsForApexCharts()
     const wrapper = mount(<OverallMaturityCategoryBreakdown loading={false} levels={levels} categoryLevelCounts={categoryLevelCounts}/>);
 
     // It renders the card's title
