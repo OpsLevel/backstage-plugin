e34e81c0ccbe300de6d7ee4ed4b1b4d34f479f95
Patrick <patrick@opslevel.com>
2023-05-25 09:41:13 -0400

    Update MarkdownViewer.test.tsx

--- src/test/MarkdownViewer.test.tsx
+++ src/test/MarkdownViewer.test.tsx
@@ -13,35 +13,35 @@ const PARSED_SHORT_TEXT = "This is a text too short to truncate.\n";
 const PARSED_MALICIOUS_TEXT = "hello how are you\n";
 
 describe('MarkdownViewer', () => {
-    it('with truncate on when there is stuff to truncate', () => {
-        const wrapper = mount(<MarkdownViewer value={ LONG_TEXT } truncate />);
-        expect(wrapper.find("span").length).toEqual(2);
-        expect(wrapper.find(".span-markdown").text()).toEqual(TRUNCATED_PARSED_LONG_TEXT);
-        expect(wrapper.find(".span-toggle-expansion").text()).toEqual("(show more)")
-    });
+  it('with truncate on when there is stuff to truncate', () => {
+    const wrapper = mount(<MarkdownViewer value={ LONG_TEXT } truncate />);
+    expect(wrapper.find("span").length).toEqual(2);
+    expect(wrapper.find(".span-markdown").text()).toEqual(TRUNCATED_PARSED_LONG_TEXT);
+    expect(wrapper.find(".span-toggle-expansion").text()).toEqual("(show more)")
+  });
 
-    it('with truncate on when there is nothing to truncate', () => {
-        const wrapper = mount(<MarkdownViewer value={ SHORT_TEXT } truncate />);
-        expect(wrapper.find("span").length).toEqual(1);
-        expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_SHORT_TEXT);
-    });
+  it('with truncate on when there is nothing to truncate', () => {
+    const wrapper = mount(<MarkdownViewer value={ SHORT_TEXT } truncate />);
+    expect(wrapper.find("span").length).toEqual(1);
+    expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_SHORT_TEXT);
+  });
 
-    it('with truncate off when there is stuff to truncate', () => {
-        const wrapper = mount(<MarkdownViewer value={ LONG_TEXT } truncate={ false } />);
-        expect(wrapper.find("span").length).toEqual(1);
-        expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_LONG_TEXT);
-    });
+  it('with truncate off when there is stuff to truncate', () => {
+    const wrapper = mount(<MarkdownViewer value={ LONG_TEXT } truncate={ false } />);
+    expect(wrapper.find("span").length).toEqual(1);
+    expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_LONG_TEXT);
+  });
 
-    it('with truncate off when there is nothing to truncate', () => {
-        const wrapper = mount(<MarkdownViewer value={ SHORT_TEXT } truncate={ false } />);
-        expect(wrapper.find("span").length).toEqual(1);
-        expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_SHORT_TEXT);
-    });
+  it('with truncate off when there is nothing to truncate', () => {
+    const wrapper = mount(<MarkdownViewer value={ SHORT_TEXT } truncate={ false } />);
+    expect(wrapper.find("span").length).toEqual(1);
+    expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_SHORT_TEXT);
+  });
 
-    it('when trying to inject a script', () => {
-        const wrapper = mount(<MarkdownViewer value={ MALICIOUS_TEXT } truncate={ false } />);
-        expect(wrapper.find("span").length).toEqual(1);
-        expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_MALICIOUS_TEXT);
-        expect(wrapper.find(".span-markdown").html()).not.toContain("script");
-    });
+  it('when trying to inject a script', () => {
+    const wrapper = mount(<MarkdownViewer value={ MALICIOUS_TEXT } truncate={ false } />);
+    expect(wrapper.find("span").length).toEqual(1);
+    expect(wrapper.find(".span-markdown").text()).toEqual(PARSED_MALICIOUS_TEXT);
+    expect(wrapper.find(".span-markdown").html()).not.toContain("script");
+  });
 });
