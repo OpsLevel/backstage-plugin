cec096c254d234da6741dc62a0d03c6462ea4e04
Mark Katerberg <katerberg@fastmail.fm>
2023-10-25 17:37:38 -0500

    Simple scorecard story

---
+++ src/components/Scorecard.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Scorecard from './Scorecard';
+// import React from 'react';
+// import { useState } from 'react';
+
+const meta = {
+  title: 'Scorecard',
+  component: Scorecard,
+} satisfies Meta<typeof Scorecard>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    levels: [{index: 0, name: "Not so great"}, {index: 1, name: "Slightly better"}, {index: 3, name: "Great"}, {index: 4, name: "Amazing"}],
+    levelCategories: [
+      {level: {name: "Not so great"}, category: {name: "Ownership"}},
+      {level: {name: "Slightly better"}, category: {name: "Reliability"}},
+      {level: {name: "Great"}, category: {name: "Observability"}},
+    ]
+  },
+};
